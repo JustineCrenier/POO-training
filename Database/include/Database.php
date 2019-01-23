ee6f5038c745f0a243619988e8e8ff68b5303030
JustineCrenier <justine.crenier@gmail.com>
2019-01-23 22:15:06 +0100

    class database créee

--- Database/include/Database.php
+++ Database/include/Database.php
@@ -2,9 +2,19 @@
 
 	class Database {
 
-		private $servername;
-		private $username;
-		private $password;
-		private $dbname;
+		private $_servername;
+		private $_username;
+		private $_password;
+		private $_dbname;
+
+		protected function connect(){
+			$this->_servername = "localhost:8889";
+			$this->_username = "root";
+			$this->_password = "root";
+			$this->_dbname = "trainingoop";
+
+			$connexion = new mysqli($this->_servername, $this->_username, $this->_password, $this->_dbname);
+			return $connexion
+		}
 
 	}
--- Database/index.php
+++ Database/index.php
@@ -1,5 +1,6 @@
 <?php
 
 	require 'include/Database.php';
+	require 'include/User.php';
 	
 ?>
