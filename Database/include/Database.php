3a3d0c5ba9c45634a5373210541db4c63a621f73
JustineCrenier <justine.crenier@gmail.com>
2019-01-23 22:02:26 +0100

    database debut

---
+++ Database/include/Database.php
@@ -0,0 +1,10 @@
+<?php
+
+	class Database {
+
+		private $servername;
+		private $username;
+		private $password;
+		private $dbname;
+
+	}
---
+++ Database/index.php
@@ -0,0 +1,5 @@
+<?php
+
+	require 'include/Database.php';
+	
+?>
